ae8e35e76ceeaca7f3098e908b4c0a9732dbbb24
alexanndrra <sashavornezh@icloud.com>
2023-09-12 11:58:56 -0500

    started numSelected() method

--- app/src/main/java/com/example/calculatorappchanda/MainActivity.java
+++ app/src/main/java/com/example/calculatorappchanda/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    int numInput1;
+    int numInput2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,51 +17,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void calculate(View v){
-        //get inputs
-        EditText num1 = findViewById(R.id.editTextInput);
-        EditText num2 = findViewById(R.id.editTextInput2);
+    public void onClick(View v){
 
-        //make inputs uneditable
-        String num1String = num1.getText().toString();
-        String num2String = num2.getText().toString();
-
-        //turn the strings to doubles for calculations
-        double input1 = Double.parseDouble(num1.toString());
-        double input2 = Double.parseDouble(num2.toString());
-
-
-        //get which operation button was pressed
-
-        //calculate
-        double output = operation(input1, input2, "+"); //replace "+" with the value from op button
-
-
-
-
-        //sync number buttons
-
-
-        //work on delete
     }
 
-    public double operation(double a, double b, String op){
-        double res = 0;
-        if(op.equals("+")){
-            res = a+b;
-        }
-        else if(op.equals("-")){
-            res = a-b;
-        }
-        else if(op.equals("*")){
-            res = a*b;
-        }
-        else if(op.equals("/")){
-            res = a/b;
-        }
-        else{
-            return -0;
-        }
-        return res;
+    //which number is pressed
+    public void numSelected(View v){
+
     }
+
+
+
+//    public double operation(double a, double b, String op){
+//        double res = 0;
+//        if(op.equals("+")){
+//            res = a+b;
+//        }
+//        else if(op.equals("-")){
+//            res = a-b;
+//        }
+//        else if(op.equals("*")){
+//            res = a*b;
+//        }
+//        else if(op.equals("/")){
+//            res = a/b;
+//        }
+//        else{
+//            return -0;
+//        }
+//        return res;
+//    }
+
 }
